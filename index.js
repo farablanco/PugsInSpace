55d798c4fd956cecbc7116b3d294afbb4a353811
Fara Aileen <Fara Aileen>
2017-04-30 15:03:19 +0800

    port changes

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var app = express();
 app.use(express.static(__dirname+"/"));
 app.use("/bower_components", express.static(__dirname + "/bower_components"));
 
-app.set("port", process.argv[2] || process.env.APP_PORT || 8080);
+const NODE_PORT = process.env.PORT || 8080;
+
+app.set("port", NODE_PORT);
 
 app.listen(app.get("port"), function(){
     console.info("Application started on port %d", app.get("port"));
